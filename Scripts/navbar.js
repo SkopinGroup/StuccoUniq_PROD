0e1d25ba780a441f0b78f5ebbf0b578e5f53544a
AntonioSkopin <askopin@outlook.com>
2022-03-08 18:43:17 +0100

    Updated website

--- Scripts/navbar.js
+++ Scripts/navbar.js
@@ -1,14 +1,29 @@
-const nav = document.getElementById("navbar");
-const nav2 = document.getElementById("navbar2");
+const desktop_nav = document.getElementById("desktop_nav");
+const mobile_nav = document.getElementById("mobile_nav");
 
 const addNavbarStyles = () => {
     if (window.scrollY === 0) {
-        nav.classList.remove("blurred-bg");
-        nav2.classList.remove("blurred-bg");
+        desktop_nav.classList.remove("blurred-bg");
+        mobile_nav.classList.remove("blurred-bg");
     } else if (window.scrollY > 0) {
-        nav.classList.add("blurred-bg");
-        nav2.classList.add("blurred-bg");
+        desktop_nav.classList.add("blurred-bg");
+        mobile_nav.classList.add("blurred-bg");
     }
 }
-
 window.addEventListener("scroll", addNavbarStyles);
+
+const open_navbar_btn = document.getElementById("open-nav-btn");
+const close_navbar_btn = document.getElementById("close-nav-btn");
+const nav_menu = document.getElementById("nav-menu");
+const open_styles = "block";
+const closed_styles = "hidden";
+
+open_navbar_btn.addEventListener("click", e => {
+    nav_menu.classList.remove(closed_styles);
+    nav_menu.classList.add(open_styles);
+});
+
+close_navbar_btn.addEventListener("click", e => {
+    nav_menu.classList.remove(open_styles);
+    nav_menu.classList.add(closed_styles);
+});
