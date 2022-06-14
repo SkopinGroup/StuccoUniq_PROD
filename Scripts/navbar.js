caa932665ad2f47cc908ae63130d47009c97149f
AntonioSkopin <askopin@outlook.com>
2022-06-14 06:41:35 +0200

    Updated website to new version

--- Scripts/navbar.js
+++ Scripts/navbar.js
@@ -1,17 +1,6 @@
 const desktop_nav = document.getElementById("desktop_nav");
 const mobile_nav = document.getElementById("mobile_nav");
 
-const addNavbarStyles = () => {
-    if (window.scrollY === 0) {
-        desktop_nav.classList.remove("blurred-bg");
-        mobile_nav.classList.remove("blurred-bg");
-    } else if (window.scrollY > 0) {
-        desktop_nav.classList.add("blurred-bg");
-        mobile_nav.classList.add("blurred-bg");
-    }
-}
-window.addEventListener("scroll", addNavbarStyles);
-
 const open_navbar_btn = document.getElementById("open-nav-btn");
 const close_navbar_btn = document.getElementById("close-nav-btn");
 const nav_menu = document.getElementById("nav-menu");
